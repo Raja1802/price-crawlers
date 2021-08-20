3c7719215ff9c5a51ad5cba3584ece3587128d66
raja1802 <rajagandla16@gmail.com>
2021-08-20 20:43:22 +0530

    Asa

--- ajar/pipelines.py
+++ ajar/pipelines.py
@@ -23,7 +23,7 @@ class AjarPipeline:
         # mongodb+srv://ajar:<password>
         # print(self)
         client = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@cluster0-shard-00-00.eyv0d.mongodb.net:27017,cluster0-shard-00-01.eyv0d.mongodb.net:27017,cluster0-shard-00-02.eyv0d.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-110jin-shard-0&authSource=admin&retryWrites=true&w=majority")
-        db = client.amazon
+        db = client.nykaa
         collect = db.reliace_prices
         collect_price = db.prices
         # collect.insert(dict(item))#
