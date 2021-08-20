92f1de917bf365e5a173d918d3f10b86755cfb83
raja1802 <rajagandla16@gmail.com>
2021-08-20 11:04:00 +0530

    asas

--- ajar/spiders/pantloonsspecs.py
+++ ajar/spiders/pantloonsspecs.py
@@ -44,26 +44,30 @@ class QuotesInfiniteScrollSpider(scrapy.Spider):
     name = "pantloons_specs_data"
     rotate_user_agent = True
     allowed_domains = ["www.pantaloons.com"]
+    myclient = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@links-shard-00-00.rjots.mongodb.net:27017,links-shard-00-01.rjots.mongodb.net:27017,links-shard-00-02.rjots.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xypyrq-shard-0&authSource=admin&retryWrites=true&w=majority")
+    mydb = myclient.LinksDB
+    mycol = mydb.Links
+    mydoc = mycol.find({"store_id": 33233})
+    df = json_normalize(mydoc)
+    
     start_urls = []
-    def start_requests(self):
-        myclient = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@links-shard-00-00.rjots.mongodb.net:27017,links-shard-00-01.rjots.mongodb.net:27017,links-shard-00-02.rjots.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xypyrq-shard-0&authSource=admin&retryWrites=true&w=majority")
-        mydb = myclient.LinksDB
-        mycol = mydb.Links
-        mydoc = mycol.find({"store_id": 33233})
-        df = json_normalize(mydoc)
-        # columns = ['product_id']
-        # for column in columns:
-        #     df[column] = df[column].astype(str)
-        #     df[column] =  "https://www.flipkart.com" + df[column]
-        # df.drop(df[df["url"] == "https://www.flipkart.comNAN"].index, inplace=True)
-        # df = pd.read_csv(r"C:\Users\G RAJA\Desktop\ajarani.me\data\exports\AnimeApi\daily\converted_csv\07-02-2021.csv")
-        # df2 = df.drop_duplicates(subset="ep_url" , keep='first')
-        # df2 = df2.iloc[60000:]
-        urls = []
-        for index, row in df.iterrows():
-            sleep(2)
-            print(index)
-            yield scrapy.Request(row["product_id"], self.parse)
+    for index, row in df.iterrows():
+        # sleep(2)
+        print(index)
+        start_urls.append(row["product_id"])
+    # start_urls = []
+    # def start_requests(self):
+    #     myclient = pymongo.MongoClient("mongodb://ajar:" + urllib.parse.quote_plus("Raja@1802") + "@links-shard-00-00.rjots.mongodb.net:27017,links-shard-00-01.rjots.mongodb.net:27017,links-shard-00-02.rjots.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-xypyrq-shard-0&authSource=admin&retryWrites=true&w=majority")
+    #     mydb = myclient.LinksDB
+    #     mycol = mydb.Links
+    #     mydoc = mycol.find({"store_id": 33233})
+    #     df = json_normalize(mydoc)
+       
+    #     urls = []
+    #     for index, row in df.iterrows():
+    #         sleep(2)
+    #         print(index)
+    #         yield scrapy.Request(row["product_id"], self.parse)
             
     # rules = (Rule(sle(allow=( "shirt", "shoes","mobile","cycle","women","men","/p/"), deny=("product-reviews")), callback="parse_result", follow=True),)
     # parsing results with below function
