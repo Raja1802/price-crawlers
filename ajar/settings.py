f11524dd622688925b8296c6b64136be77049e9e
raja1802 <rajagandla16@gmail.com>
2021-08-31 17:37:41 +0530

    as

--- ajar/settings.py
+++ ajar/settings.py
@@ -17,7 +17,7 @@ SPIDER_MODULES = ["ajar.spiders"]
 NEWSPIDER_MODULE = "ajar.spiders"
 #
 
-
+#
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'ajar (+http://www.yourdomain.com)'
 # USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36"
