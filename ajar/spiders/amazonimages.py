ccb0a85c8abe7eba8a190a1733b6ec9903473dc2
raja1802 <rajagandla16@gmail.com>
2021-09-05 11:16:21 +0530

    Asas

--- ajar/spiders/amazonimages.py
+++ ajar/spiders/amazonimages.py
@@ -25,7 +25,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
-
+#
 class QuotesInfiniteScrollSpider(scrapy.Spider):
     name = "amazon_images_data"
     rotate_user_agent = True
